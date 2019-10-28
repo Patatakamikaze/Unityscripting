5cdb3b1a205c71e36f3bb048f498745be64d89d7
PepeBarrientos <pepebarrientosmoreno@gmail.com>
2019-10-28 16:54:36 +0100

    Parte hello world

---
+++ Assets/Scripts/Helloworld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello pepe");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hola");
+    }
+}
